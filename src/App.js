bb4d8c0b726f826757e7b59447a8cd5464e8ab61
Christopher Martin <martincdev@gmail.com>
2018-08-28 16:15:41 -0500

    add Clarifai package to app, create const for api key, update gitignore

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
 import './App.css';
 import 'tachyons';
+
+const API_KEY = `${process.env.REACT_APP_CLARIFAI_API_KEY}`;
+
+const app = new Clarifai.App({
+  apiKey: `${API_KEY}`
+});
 
 const particlesOptions = {
   particles: {
@@ -21,6 +28,32 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg")
+      .then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
@@ -31,7 +64,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/*<FaceRecognition />}*/}
       </div>
     );
