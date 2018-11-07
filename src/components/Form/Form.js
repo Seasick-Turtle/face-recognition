f95ff672aa2c7ae1174986abe24dfbd1e490a94c
Christopher Martin <martincdev@gmail.com>
2018-11-06 23:07:30 -0600

    update functions for submitting

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -67,11 +67,11 @@ export const Password = ({ onPasswordChange }) => {
 };
 
 
-export const Submit = ({ }) => {
+export const RegisterSubmit = ({ onSubmitSignIn }) => {
   return (
     <div>
       <input
-        onClick={this.onSubmitSignIn}
+        onClick={onSubmitSignIn}
         className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
         type="submit"
         value="Register" />
@@ -79,35 +79,32 @@ export const Submit = ({ }) => {
   )
 };
 
-export const Form = ({ inputType }) => {
+export const SignInSubmit = ({ onSubmitSignIn, onRouteChange }) => {
+  return (
+    <React.Fragment>
+      <div>
+        <input
+          onClick={onSubmitSignIn}
+          className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+          type="submit"
+          value="Sign in" />
+      </div>
+      <div className="lh-copy mt3">
+        <p
+          onClick={() => onRouteChange('register')}
+          className="f6 link dim black db b pointer">
+          Register
+        </p>
+      </div>
+    </React.Fragment>
+  )
+};
+
+export const Form = () => {
 
   return (
-    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-      <main className="pa4 black-80">
-        <div className="measure">
-          <fieldset
-            id="sign_up"
-            className="ba b--transparent ph0 mh0">
-            {props.children}
-
-
-
-
-
-          </fieldset>
-
-          <div>
-            <input
-              onClick={this.onSubmitSignIn}
-              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-              type="submit"
-              value="Register" />
-          </div>
-
-        </div>
-      </main>
-    </article>
-
-
+    <React.Fragment>
+      {props.children}
+    </React.Fragment>
   )
 };
